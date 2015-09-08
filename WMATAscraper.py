9af64fcf1b576ac777e0944ca80f0afc3a75d7d9
Andrew Yue <andrewyue@users.noreply.github.com>
2015-09-08 09:13:58 -0400

    Script works; error handling to be fixed

--- WMATAscraper.py
+++ WMATAscraper.py
@@ -1,14 +1,46 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
-#BASE = 'http://wmata.com/rail/service_reports/'
+# write out a csv file containing every incident from the archive, a timestamp,
+# type of delay, line information, station, and delay time (if applicable)
 
-#'http://wmata.com/rail/service_reports/viewReportArchive.cfm'
-#links = soup.find("ul", "links2")
+def getLinks(URL):
+    BASE = 'http://wmata.com/rail/service_reports/'
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.text,'html')
+    allLinks = soup.find('ul','links2')
+    dates = [link.string for link in allLinks.findAll('a')]
+    links = [BASE + str(link['href']) for link in allLinks.findAll('a')]
+    return zip(dates, links)
 
 def getDelays(URL):
     r = requests.get(URL)
     soup = BeautifulSoup(r.text,'html')
     div = soup.find('div','internal-box2-inner')
-    delays = [p for p in div.findAll('p')]
+    delays = [p.string for p in div.findAll('p')]
+    # last three <p></p> instances are junk
     return delays[:-3]
+
+archive='http://wmata.com/rail/service_reports/viewReportArchive.cfm'
+datesLinks = getLinks(archive)
+delayTime = []
+delayMSG = []
+for dateLink in datesLinks:
+    # pitfalls - entries with embedded hyperlinks, empty entries
+    # need to fix more elegantly
+    delays = getDelays(dateLink[1])
+    for delay in delays:
+        try:
+            delayTime.append(pd.Timestamp(dateLink[0]+ ' ' + delay.split()[0] + ' ' + delay.split()[1]))
+            delayMSG.append(' '.join(delay.split()[2:]))
+        except AttributeError:
+            pass
+        except IndexError:
+            pass
+        except ValueError:
+            pass
+
+df_dict = {'timestamp' : delayTime, 'message' : delayMSG}
+df = pd.DataFrame(df_dict)
+df.to_csv('~/Projects/WMATAscraper/test.csv')
